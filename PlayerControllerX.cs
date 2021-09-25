e62030ffcfc8eee8702502e5fb6b4a26b8841467
Don <50916200+Kibetdonald@users.noreply.github.com>
2021-09-25 07:18:52 +0300

    Added C# Scripts

---
+++ PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed = .2f;
+    private float verticalInput;
+    private float rotationSpeed = 40f;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * speed);
+        transform.Rotate(Vector3.right * Time.deltaTime * -verticalInput * rotationSpeed);
+    }
+}
+
